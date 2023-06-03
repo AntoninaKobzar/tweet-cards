a4176e6f89b1a224f9b01fc30a2f8f776f542dd7
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-06-03 10:03:16 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "components/App";
+// import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "redux/store";
+// import { ChakraProvider } from "@chakra-ui/react";
+import { PersistGate } from "redux-persist/integration/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
