15f3d433de368a3a187c0ad8e865004387d20ff6
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-06-03 10:32:36 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "components/App";
-// import "./index.css";
-import { Provider } from "react-redux";
-import { store, persistor } from "redux/store";
-// import { ChakraProvider } from "@chakra-ui/react";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/goit-react-hw-08-phonebook">
-            <App />
-          </BrowserRouter>
-        </PersistGate>
-      </Provider>
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
